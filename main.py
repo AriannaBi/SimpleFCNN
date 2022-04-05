3305b91f68d0cc0f1a174ecf64581bb6396cc3e0
aribianchi <55762431+aribianchi@users.noreply.github.com>
2022-04-05 11:09:59 +0200

    create sampling of 4Hz per second

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import pyarrow.parquet as pq
 import numpy as np
 import csv
+import tensorflow as tf
 
 
 def read_data_locally():
@@ -10,7 +11,7 @@ def read_data_locally():
     head = table.head()
     # print(table)
     # print(head)
-    types = table.dtypes
+    # types = table.dtypes
     # print(types)
     # timestamp = table.pop('timestamp')
     # print(timestamp)
@@ -18,34 +19,45 @@ def read_data_locally():
 
     # creating DataFrame
     df = pd.DataFrame(table)
-    # print(df)
+
     # converting timestamp
     timestamp_col = pd.to_datetime(df['timestamp'], unit='s')
     df['timestamp'] = timestamp_col
-    # print(df)
+    len = df.shape[0]
+
     # write to csv file the date (I want to temporally visualize it)
-    with open('original.csv', 'w') as csvfile:
-        fieldnames = ['Date','X','Y','Z']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        for i in range(0, 50):
-            writer.writerow({'Date': df['timestamp'][i], 'X': df['X'][i] , 'Y': df['Y'][i], 'Z':df['Z'][i]})
+    # with open('original.csv', 'w') as csvfile:
+    #     fieldnames = ['Date','X','Y','Z']
+    #     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    #     writer.writeheader()
+    #     for i in range(0, 20):
+    #         writer.writerow({'Date': df['timestamp'][i], 'X': df['X'][i] , 'Y': df['Y'][i], 'Z':df['Z'][i]})
+
 
     # downsampling
-    df = df.resample('10s', on='timestamp').first()
-    # print(df)
+    df = df.resample('250ms', on='timestamp').sum()
+    # print(df.shape[0])
+    df = df.reset_index()
+    # ----------------------------------- write on file parquet ------------------------------------------------
+    # table = df.head(20)
+    # table.to_parquet('filep.parquet')
+    # ----------------------------------------------------------------------------------------------------------
+
+
     with open('modified.csv', 'w') as csvfile:
         fieldnames = ['Date', 'X', 'Y', 'Z']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
-        for i in range(0, 50):
+        for i in range(0, 10000):
             writer.writerow({'Date': df['timestamp'][i], 'X': df['X'][i] , 'Y': df['Y'][i], 'Z':df['Z'][i]})
+
+    # can't update tensors, only create
+    
 
 
 
 if __name__ == "__main__":
     try:
         read_data_locally()
-        # read_data_OneDrive()
     except Exception as message:
         print(f"MY FAULT")
