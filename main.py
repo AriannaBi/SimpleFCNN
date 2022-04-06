76836911c4e0bc2145f3ff2c1a0fe8d5d9b06fd1
aribianchi <55762431+aribianchi@users.noreply.github.com>
2022-04-06 11:45:58 +0200

    Create Tensor with pytorch

--- main.py
+++ main.py
@@ -3,61 +3,91 @@ import pyarrow.parquet as pq
 import numpy as np
 import csv
 import tensorflow as tf
+import torch
+from enum import Enum
+
+def type_file(name):
+    print("type_file")
+    type_file = name.split('/')[2][:3]
+    match type_file:
+        case 'EDA':
+            n = 1
+            return 0, n
+        case 'BVP':
+            n = 2
+            return '500ms', n
+        case 'ACC':
+            n = 3
+            return '250ms', n
+        case 'ST':
+            n = 4
+            return 0, n
+    return 0, 0
+
+def read_data_locally(list_file):
+    print("read_data")
+    for name_file in list_file:
+        # DataFrame
+        table = pd.read_parquet(name_file, engine='pyarrow')
+
+        # creating DataFrame
+        df = pd.DataFrame(table)
+
+        # converting timestamp
+        timestamp_col = pd.to_datetime(df['timestamp'], unit='s')
+        df['timestamp'] = timestamp_col
 
 
-def read_data_locally():
-    # DataFrame
-    table = pd.read_parquet('./ACC_22.parquet', engine='pyarrow')
-    head = table.head()
-    # print(table)
-    # print(head)
-    # types = table.dtypes
-    # print(types)
-    # timestamp = table.pop('timestamp')
-    # print(timestamp)
+        arr_type = type_file(name_file)
+
+        # create 6 matrices with rows and cols (TENSOR)
+        # i - z axis            6
+        # j - y axis (rows)     df.shape[0]
+        # k - x axis (cols)     2400
+        tensor = torch.empty((6, df.shape[0], 2400), dtype=torch.float64)
 
 
-    # creating DataFrame
-    df = pd.DataFrame(table)
-
-    # converting timestamp
-    timestamp_col = pd.to_datetime(df['timestamp'], unit='s')
-    df['timestamp'] = timestamp_col
-    len = df.shape[0]
-
-    # write to csv file the date (I want to temporally visualize it)
-    # with open('original.csv', 'w') as csvfile:
-    #     fieldnames = ['Date','X','Y','Z']
-    #     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    #     writer.writeheader()
-    #     for i in range(0, 20):
-    #         writer.writerow({'Date': df['timestamp'][i], 'X': df['X'][i] , 'Y': df['Y'][i], 'Z':df['Z'][i]})
+        # RESAMPLE the tensor
+        # if 0, it's already 4Hz, otherwise it's either '250ms' or '500ms'
+        if arr_type[0] != 0:
+            df = df.resample(arr_type[0], on='timestamp').mean()
+            df = df.reset_index()
 
 
-    # downsampling
-    df = df.resample('250ms', on='timestamp').sum()
-    # print(df.shape[0])
-    df = df.reset_index()
-    # ----------------------------------- write on file parquet ------------------------------------------------
-    # table = df.head(20)
-    # table.to_parquet('filep.parquet')
-    # ----------------------------------------------------------------------------------------------------------
+
+        # for i in range(6):
+        #     for j in range(df.shape[0]):
+        #         for k in range(2400):
+                    # print(tensor[i][j][k])
 
 
-    with open('modified.csv', 'w') as csvfile:
-        fieldnames = ['Date', 'X', 'Y', 'Z']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        for i in range(0, 10000):
-            writer.writerow({'Date': df['timestamp'][i], 'X': df['X'][i] , 'Y': df['Y'][i], 'Z':df['Z'][i]})
+        # if ACC:
+        #
+        #
+        #     ACC_X = df['X'].tolist()
+        #     ACC_Y = df['Y'].tolist()
+        #     ACC_Z = df['Z'].tolist()
+        #
+        # else if EDA:
+        #     EDA = df['EDA'].tolist()
+        # else if BVP
+        #     BVP = df['Z'].tolist()
+        # else if TEMP
+        #     TEMP = df['Z'].tolist()
 
-    # can't update tensors, only create
+
+        with open('modified.csv', 'w') as csvfile:
+            fieldnames = ['Date', 'X', 'Y', 'Z']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            for i in range(0, df.shape[0]):
+                writer.writerow({'Date': df['timestamp'][i], 'X': df['X'][i] , 'Y': df['Y'][i], 'Z':df['Z'][i]})
+
     
-
-
 
 if __name__ == "__main__":
     try:
-        read_data_locally()
+        list_files = ['./files/ACC_22.parquet']
+        read_data_locally(list_files)
     except Exception as message:
         print(f"MY FAULT")
